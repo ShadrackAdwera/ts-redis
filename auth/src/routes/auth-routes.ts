1fe9ee1bcd76cd2fd17315ec23164ba83c1a0e88
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-20 15:15:34 +0300

    configure custom error messages on validation fail

--- auth/src/routes/auth-routes.ts
+++ auth/src/routes/auth-routes.ts
@@ -36,21 +36,37 @@ router.post(
 router.post(
   '/login',
   [
-    body('email').normalizeEmail().isEmail(),
-    body('password').trim().isLength({ min: 6 }),
+    body('email')
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Please provide a valid email'),
+    body('password')
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters'),
   ],
   login
 );
 
 router.patch(
   '/request-reset-token',
-  [body('email').normalizeEmail().isEmail()],
+  [
+    body('email')
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Please provide a valid email'),
+  ],
   requestPasswordReset
 );
 
 router.patch(
   '/reset-password/:resetToken',
-  [body('password').trim().isLength({ min: 6 })],
+  [
+    body('password')
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters'),
+  ],
   resetPassword
 );
 
@@ -58,15 +74,32 @@ router.use(checkAuth);
 router.post(
   '/new-user',
   [
-    body('username').trim().not().isEmpty(),
-    body('email').normalizeEmail().isEmail(),
-    body('role').trim().not().isEmpty(),
+    body('username')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please provide your username'),
+    body('email')
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Please provide a valid email'),
+    body('role')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Provide a role for this user'),
   ],
   addUsers
 );
 router.patch(
   '/modify-user/:userId',
-  [body('userRole').trim().not().isEmpty()],
+  [
+    body('userRole')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Provide a role for this user'),
+  ],
   modifyUserRole
 );
 
