87ef8f7cde3c05daf6ef2677018e77f614e9f424
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-03 17:20:23 +0300

    create usefetch tasks hook

---
+++ client/src/hooks/useFetchTasks.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { queryKeys, TTasks } from '../utils/types';
+import useUser from './useUser';
+
+const fetchTasks = async (
+  token: string | undefined,
+  signal: AbortSignal | undefined
+): Promise<TTasks[]> => {
+  const { data }: AxiosResponse<TTasks[]> = await axios.get('/api/tasks', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    signal,
+  });
+
+  return data;
+};
+
+const useFetchTasks = () => {
+  const { userInfo } = useUser();
+  const { data } = useQuery(
+    [queryKeys.TASKS],
+    ({ signal }) => fetchTasks(userInfo?.token, signal),
+    {
+      enabled: !!userInfo,
+    }
+  );
+  return data;
+};
+
+export default useFetchTasks;
