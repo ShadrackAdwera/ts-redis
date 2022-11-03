2ab5997781600c45aa6de0fb827d933afffaae7b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-03 17:56:39 +0300

    set up tasks to display on homepage

--- client/src/components/starting-page/StartingPageContent.tsx
+++ client/src/components/starting-page/StartingPageContent.tsx
@@ -1,9 +1,27 @@
+import { Grid } from '@mui/material';
+
+import useFetchTasks from '../../hooks/useFetchTasks';
+import TasksCard from '../Card/Card';
 import classes from './StartingPageContent.module.css';
 
 const StartingPageContent = () => {
+  const { tasks } = useFetchTasks();
   return (
     <section className={classes.starting}>
       <h3>React Query Rocks!</h3>
+      <Grid container spacing={3}>
+        {tasks.map((task) => (
+          <Grid key={task.id} item xs={12} sm={12} md={6} lg={6}>
+            <TasksCard
+              createdAt={new Date().toDateString()}
+              description={task.description}
+              title={task.title}
+              status={task.status}
+              image={task.image}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </section>
   );
 };
